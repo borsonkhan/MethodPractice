5b7b78eced1884e270b8955f405df682a78883b1
Borson Khan <borsonkh@gmail.com>
2017-12-16 13:18:48 -0800

    add new method

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -4,7 +4,29 @@ public class MethodPractice {
 
 	public static void main (String [] args) {
 		
+		System.out.print("Q1 = " + getText("khan"));
 		
 	}
+	
+	
+	
+ /*
+  * Que 1 ;
+  * input = String name;
+  * return = String;
+  * serving bucket = variable;
+  */
+	
+	public static String getText(String name) {
+		String text = " ";
+		
+		
+		return text;
+	}
+	
+	
+	
+	
+	
 	
 }
