56e15cb3570c4392de50ac4b41344b8dce1d4d25
Borson Khan <borsonkh@gmail.com>
2017-12-17 03:03:40 -0800

    add number 4 method

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -1,62 +1,70 @@
 package methods;
 
+import java.util.ArrayList;
+
 public class MethodPractice {
 
-	public static void main (String [] args) {
-		
+	public static void main(String[] args) {
+
 		System.out.println("Q1 = " + getText("khan"));
 		System.out.println("Q2 = " + getSubject("Khan"));
 		System.out.println("Q2 = " + getUpperText("Khan"));
-		
-		
+		System.out.println("Q4 = " + getEvenNumber(20));
+
 	}
-	
-	
-	
- /*
-  * Que 1 ;
-  * input = String name;
-  * return = String;
-  * serving bucket = variable;
-  */
-	
+
+	/*
+	 * Que 1 ; input = String name; return = String; serving bucket = variable;
+	 */
+
 	public static String getText(String name) {
 		String text = " ";
-		
+
 		text = name;
-		
+
 		return text;
 	}
-	
+
 	/*
-	 * Q2;
-	 * input = String name;
-	 * return = String ;
-	 * serving bucket = variable;
+	 * Q2; input = String name; return = String ; serving bucket = variable;
 	 */
-	
+
 	public static String getSubject(String name) {
 		String subject = " ";
-		
+
 		subject = name.toLowerCase();
-		
+
 		return subject;
 	}
-	
+
 	/**
-	 * Q3;
-	 * input = String name;
-	 * return type = String;
-	 * serving bucket = variable;
+	 * Q3; input = String name; return type = String; serving bucket = variable;
 	 */
-	
+
 	public static String getUpperText(String name) {
 		String upperText = " ";
-		
+
 		upperText = name.toUpperCase();
-		
+
 		return upperText;
 	}
-	
-	
+
+	/*
+	 * Q4; input = int number; return type = ArrayList<Integer>; serving bucket =
+	 * list;
+	 */
+	public static ArrayList<Integer> getEvenNumber(int number) {
+		ArrayList<Integer> evenNumber = new ArrayList<Integer>();
+
+		for (int i = 0; i <= 20; i++) {
+
+			if (i % 2 == 0) {
+
+				evenNumber.add(i);
+			}
+		}
+
+		return evenNumber;
+	}
+
 }
