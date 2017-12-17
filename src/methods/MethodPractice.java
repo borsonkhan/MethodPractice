43f7e0d0e765adf572080be0003abe1e55557c30
Borson Khan <borsonkh@gmail.com>
2017-12-17 02:55:04 -0800

    add number 3 method

--- src/methods/MethodPractice.java
+++ src/methods/MethodPractice.java
@@ -5,7 +5,9 @@ public class MethodPractice {
 	public static void main (String [] args) {
 		
 		System.out.println("Q1 = " + getText("khan"));
-		System.out.println("Q2 = " + getSubject("khan"));
+		System.out.println("Q2 = " + getSubject("Khan"));
+		System.out.println("Q2 = " + getUpperText("Khan"));
+		
 		
 	}
 	
@@ -36,11 +38,25 @@ public class MethodPractice {
 	public static String getSubject(String name) {
 		String subject = " ";
 		
+		subject = name.toLowerCase();
 		
 		return subject;
 	}
 	
+	/**
+	 * Q3;
+	 * input = String name;
+	 * return type = String;
+	 * serving bucket = variable;
+	 */
 	
+	public static String getUpperText(String name) {
+		String upperText = " ";
+		
+		upperText = name.toUpperCase();
+		
+		return upperText;
+	}
 	
 	
 }
